830634dd6425b062ee7f201aa042a2d27567fd6f
Ken <me@kencation.com>
2025-12-16 17:50:28 -0600

    Update PdfOptimizerLogger.php

--- src/PdfOptimizerLogger.php
+++ src/PdfOptimizerLogger.php
@@ -8,21 +8,21 @@ use Stringable;
 
 class PdfOptimizerLogger implements LoggerInterface
 {
-    public function emergency(Stringable|string $message, array $context = []): void {}
+    public function emergency(Stringable|string $message, array $context = [])
 
-    public function alert(Stringable|string $message, array $context = []): void {}
+    public function alert(Stringable|string $message, array $context = [])
 
-    public function critical(Stringable|string $message, array $context = []): void {}
+    public function critical(Stringable|string $message, array $context = [])
 
-    public function error(Stringable|string $message, array $context = []): void {}
+    public function error(Stringable|string $message, array $context = [])
 
-    public function warning(Stringable|string $message, array $context = []): void {}
+    public function warning(Stringable|string $message, array $context = [])
 
-    public function notice(Stringable|string $message, array $context = []): void {}
+    public function notice(Stringable|string $message, array $context = [])
 
-    public function info(Stringable|string $message, array $context = []): void {}
+    public function info(Stringable|string $message, array $context = [])
 
-    public function debug(Stringable|string $message, array $context = []): void {}
+    public function debug(Stringable|string $message, array $context = [])
 
-    public function log($level, Stringable|string $message, array $context = []): void {}
+    public function log($level, Stringable|string $message, array $context = [])
 }
